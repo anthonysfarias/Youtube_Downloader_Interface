40f7e786dfeb789784e05e88106bc2f836a25465
KomuraAK <akileslive.farias10@gmail.com>
2023-05-12 17:29:24 -0300

    Adicionando toast

--- src/VideoDownloader.tsx
+++ src/VideoDownloader.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const VideoDownloader: React.FC = () => {
   const [videoUrl, setVideoUrl] = useState<string>("");
@@ -9,11 +11,43 @@ const VideoDownloader: React.FC = () => {
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log(`Downloading video from ${videoUrl}`);
+    if (videoUrl) {
+      const promise = new Promise<void>((resolve) => {
+        // Simulando uma operação assíncrona
+        setTimeout(() => {
+          resolve();
+        }, 2000);
+      });
+
+      toast.promise(
+        promise,
+        {
+          pending: "Downloading video...",
+          success: "Video downloaded successfully!",
+          error: "Failed to download video.",
+        }
+      );
+    } else {
+      toast.error("Video URL is required", {
+        position: toast.POSITION.TOP_RIGHT,
+        autoClose: 2000,
+      });
+    }
   };
 
   return (
     <div className="flex justify-center items-center h-screen bg-gradient-to-r from-purple-500 to-indigo-500">
+      <ToastContainer
+        position="top-right"
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
       <form
         onSubmit={handleSubmit}
         className="md:max-w-md md:w-full md:bg-white md:bg-opacity-30 md:backdrop-filter md:backdrop-blur-md md:rounded-md md:shadow-md md:p-6 lg:max-w-lg lg:w-full lg:bg-white lg:bg-opacity-30 lg:backdrop-filter lg:backdrop-blur-lg lg:rounded-lg lg:shadow-md lg:p-6"
